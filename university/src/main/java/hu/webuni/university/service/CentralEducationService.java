2c2edc7011f4fb54a570f4381c893f2ee860c7cc
ferencjozsef <jozsef.ferenc.0723@gmail.com>
2022-03-14 14:21:11 +0100

    homework5 1 feladat university módosítása

--- university/src/main/java/hu/webuni/university/service/CentralEducationService.java
+++ university/src/main/java/hu/webuni/university/service/CentralEducationService.java
@@ -1,23 +1,27 @@
 package hu.webuni.university.service;
 
-import java.util.Random;
+//import java.util.Random;
 
 import org.springframework.stereotype.Service;
 
+import hu.webuni.eduservice.wsclient.StudentXmlWsImplService;
 import hu.webuni.university.aspect.Retry;
 
 @Service
 public class CentralEducationService {
 
-	private Random random = new Random();
+//	private Random random = new Random();
 	
 	@Retry(times = 5, waitTime = 500)
 	public int getFreeSemestersForStudent(int eduStudentId) {
-		int randomTimeOut = random.nextInt(0,2);
-		if (randomTimeOut == 0) {
-			throw new RuntimeException("Central Education Service time out");
-		} else {
-			return random.nextInt(0,10);
-		}
+//		int randomTimeOut = random.nextInt(0,2);
+//		if (randomTimeOut == 0) {
+//			throw new RuntimeException("Central Education Service time out");
+//		} else {
+//			return random.nextInt(0,10);
+//		}
+		return new StudentXmlWsImplService()
+				.getStudentXmlWsImplPort()
+				.getFreeSemesterByStudent(eduStudentId);
 	}
 }
