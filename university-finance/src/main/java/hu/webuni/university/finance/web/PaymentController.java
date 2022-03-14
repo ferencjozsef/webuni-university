12756f71f743dc5510f3787b7ca097e61343973f
ferencjozsef <jozsef.ferenc.0723@gmail.com>
2022-03-14 15:58:23 +0100

    homework5 2 feladat új pénzügyi spring boot alkalmazás

---
+++ university-finance/src/main/java/hu/webuni/university/finance/web/PaymentController.java
@@ -0,0 +1,22 @@
+package hu.webuni.university.finance.web;
+
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import hu.webuni.university.finance.dto.PaymentDto;
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+public class PaymentController {
+	
+	private final JmsTemplate jmsTemplate;
+
+	@PostMapping("/api/payments")
+	public void registerPayment(@RequestBody PaymentDto payment) {
+		System.out.println("studentId: " + payment.getStudentId() + " amount: " + payment.getAmount());
+		this.jmsTemplate.convertAndSend("payments", payment);
+	}
+}
